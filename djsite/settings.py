dd2cd5a9db8c2d76f03c4176c14621f5156920a5
B4lex <b4lexxx@gmail.com>
2020-05-11 14:40:13 +0300

    Debug is true

--- djsite/settings.py
+++ djsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '42c*$z^u4y#h-w3rr=kh4adw5m=m*z@%_j+u=0nx!a$sf!grho'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
